79431fa58e44f6ac16a93e4eac9ab6aa45a3903e
Adrian Garcia Saaib <garciasaaib@gmail.com>
2020-06-24 11:12:36 -0600

    adding all basic front

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import '../assets/styles/components/CarouselItem.scss'
+import iconPlus from '../assets/static/plus-icon.png'
+import iconPlay from '../assets/static/play-icon.png'
 
 const CarouselItem = () => (
   <div className="carousel-item">
     <img className="carousel-item__img" src="https://images.pexels.com/photos/789822/pexels-photo-789822.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" />
     <div className="carousel-item__details">
       <div>
-      <img className="carousel-item__details--img" src="../assets/play-icon.png" alt="Play Icon" /> 
-      <img className="carousel-item__details--img" src="../assets/plus-icon.png" alt="Plus Icon" /> 
+      <img className="carousel-item__details--img" src={iconPlay} alt="Play Icon" /> 
+      <img className="carousel-item__details--img" src={iconPlus} alt="Plus Icon" /> 
       </div>
       <p className="carousel-item__details--title">Título descriptivo</p>
       <p className="carousel-item__details--subtitle">2019 16+ 114 minutos</p>
